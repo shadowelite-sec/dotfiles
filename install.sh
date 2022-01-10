3d9381216a15cc5e4bf6f6dfee6f5a09573f7257
shadowelite <alansedp@gmail.com>
2022-01-10 11:01:06 +0530

    edited install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+RED="\e[31m"
+GREEN="\e[32m"
+END="\e[0m"
+YELLOW="\e[33m"
+MAGENTA="\e[35m"
+BOLDGREEN="\e[1;${GREEN}m"
+ITALICRED="\e[3;${RED}m"
 
 #install depen*
+echo -e "\n${RED}Installing Dependencies....${END}"
 
 sudo apt update -y && sudo apt install bspwm sxhkd polybar dmenu picom kitty feh -y
 
 #make config dir
+
 if [ -f ~/.config ]
 then
 	mkdir ~/.config
 else
-	echo -e "\nFile already exist. skiping..."
+	echo -e "\n${RED}File already exist. skiping....${END}"
 fi
 
 #cp all configs
+echo -e "\n${GREEN}Copying Config Files....${END}"
 cp -r config/* ~/.config
 
 #wall
-
+echo -e "\n${GREEN}Copying walls....${END}"
 cp -r wall/ ~/Pictures/
 
 
