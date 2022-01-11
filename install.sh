b4fc28d4694f684bd8e512fee7dcf9d700bb78fa
shadowelite <alansedp@gmail.com>
2022-01-11 16:59:10 +0530

    added fonts

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ MAGENTA="\e[35m"
 BOLDGREEN="\e[1;${GREEN}m"
 ITALICRED="\e[3;${RED}m"
 
+#dirs
+
+DIR=`pwd`
+FDIR="$HOME/.local/share/fonts"
+
 #install depen*
 echo -e "\n${RED}Installing Dependencies....${END}"
 
@@ -22,9 +27,26 @@ else
 	echo -e "\n${RED}File already exist. skiping....${END}"
 fi
 
+
+#Installing Fonts
+echo -e "\n${YELLOW}Installing fonts...${END}"
+if [[ -d "$FDIR" ]]
+then
+	cp -rf $DIR/.local/share/fonts/* "$FDIR"
+else
+	mkdir -p "$FDIR"
+	cp -rf $DIR/.local/share/fonts/* "$FDIR"
+fi
+
+
 #cp all configs
 echo -e "\n${GREEN}Copying Config Files....${END}"
 cp -r config/* ~/.config
+
+
+#wall
+echo -e "\n${GREEN}Copying walls....${END}"
+cp -r wall/ ~/Pictures/
 
 #Install zsh
 
@@ -33,9 +55,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 #cp .zshrc ~/
 #cp -r .oh-my-zsh ~/
 
-
-#wall
-echo -e "\n${GREEN}Copying walls....${END}"
-cp -r wall/ ~/Pictures/
 
 
